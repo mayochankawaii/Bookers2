00648e513941ae733e83619c25bb567f8a738efb
mayochankawaii <171436876+mayochankawaii@users.noreply.github.com>
2024-06-21 18:42:28 +0000

    Bookers2

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,17 @@
 class UsersController < ApplicationController
+  has_one_attached :image
+  
+  def new
+    @user = User.new
+  end
+  
+  def create
+    @user = User.new(post_image_params)
+    @user.user_id = current_user.id
+    @user.save
+    redirect_to post_images_path
+  end
+  
   def index
   end
 
@@ -13,4 +26,10 @@ class UsersController < ApplicationController
 
   def destroy
   end
+  
+  private
+  def list_params
+    params.require(:list).permit(:title, :image, :body)
+  end
+    
 end
