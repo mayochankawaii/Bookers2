acc1fce7ca9425f26c8359e875593a6d8c9e0bb4
mayochankawaii <171436876+mayochankawaii@users.noreply.github.com>
2024-06-25 06:53:32 +0000

    Bookers2

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,9 +1,9 @@
 class BooksController < ApplicationController
-  
+
   def new
     @book = Book.new
   end
-  
+
   def index
     @books = Book.all
     @book = Book.new
@@ -19,16 +19,18 @@ class BooksController < ApplicationController
   def create
     @book = Book.new(book_params)
     @book.user_id = current_user.id
-    @book.save
+    if @book.save
     redirect_to book_path(@book.id)
+  else
+    render :index
+  end
   end
 
   def edit
     @book = Book.find(params[:id])
   end
-  
+
   def update
-    
     @book = Book.find(params[:id])
     if @book.update(book_params)
     flash[:notice] = "You have created book successfully."
@@ -38,14 +40,14 @@ class BooksController < ApplicationController
     render :edit
     end
   end
-  
+
   def destroy
     book = Book.find(params[:id])
     book.destroy
     flash[:notice] = "Book was successfully destroyed."
     redirect_to '/books'
   end
-  
+
   private
 
   def book_params
